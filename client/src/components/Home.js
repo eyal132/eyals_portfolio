d4e315d8cef836f0cff6e95b938a10dd6d040479
eyal132 <38598729+eyal132@users.noreply.github.com>
2024-10-02 11:38:57 +1300

    CSS updates

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import '../App.css';
 import Navbar from './Nav';
-import Arrow from '../media/arrow_white.png';
 import eyalIMG from '../media/eyal-portrait.png';
 import spaceIMG from '../media/space_game.png';
 
 function Home() {
   return (
-    <section>
+    <section className="page">
     <div className="App" >
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <Navbar />
@@ -20,65 +19,64 @@ function Home() {
         <button className="contactbtn">Contact</button>
         </div>
 
-        <h3>My Journey</h3>
-        <img className="arrowIMG" src={Arrow} />
-        <img className="eyalIMG" src={eyalIMG} />
+        <h3><a href="#projects">My Journey</a></h3>
+        <img className="eyalIMG" src={eyalIMG} alt="Eyal"  />
       </header>
     </div>
     <body>
-    <table>
+    <table id="projects">
       <tr>
-      <th colspan="2" className='tableTitle'>2024</th>
+      <th className='tableTitle'>2024</th>
       </tr>
+  <div className = "project-container">
   <tr>
     <th>Capstone Website "Cornerstone"</th>
-    <th>Space Game</th>
   </tr>
   <tr>
-    <td><img className="tableIMG" src={spaceIMG} /><p>Some test describing this thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj dfogj fdoigjf ojgoifd oifdj oigfdj  oigdjroig df oi gjfdoig fdo jfodj oifdj oifd oifd go idf oi gfdoig jfdoi foidsj foisdj foisd f</p></td>
-    <td><img className="tableIMG" src={spaceIMG} /><p>Some test describing dfgoidfgsdfoijsdoifjsd oisdj oisdj f oisdj oisd oi sdoif jsdoi sdoi odf josdf osd oisd f oisdj f oisd joi oif jsodjf oisdf oisd f jthis thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdsdfosdf sod jfosd ois ofjsdo fosd fosdf osdjoif sd ffsgi jfdogo dfgoi foigjdfoi fdg oifdj oifdoi gdf oi dfo dofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj foidsj foisdj foisd f</p></td>
+    <td><img className="tableIMG" src={spaceIMG} alt="Project" /><p>Some test describing this thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj dfogj fdoigjf ojgoifd oifdj oigfdj  oigdjroig df oi gjfdoig fdo jfodj oifdj oifd oifd go idf oi gfdoig jfdoi foidsj foisdj foisd f</p></td>
   </tr>
+  </div>
+  <div className = "project-container">
   <tr>
     <th>Radiation Game</th>
-    <th>Cloud Storage</th>
   </tr>
   <tr>
-    <td><img className="tableIMG" src={spaceIMG} /><p>Some test describing this thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj dfogj fdoigjf ojgoifd oifdj oigfdj  oigdjroig df oi gjfdoig fdo jfodj oifdj oifd oifd go idf oi gfdoig jfdoi foidsj foisdj foisd f</p></td>
-    <td><img className="tableIMG" src={spaceIMG} /><p>Some test describing dfgoidfgsdfoijsdoifjsd oisdj oisdj f oisdj oisd oi sdoif jsdoi sdoi odf josdf osd oisd f oisdj f oisd joi oif jsodjf oisdf oisd f jthis thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdsdfosdf sod jfosd ois ofjsdo fosd fosdf osdjoif sd ffsgi jfdogo dfgoi foigjdfoi fdg oifdj oifdoi gdf oi dfo dofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj foidsj foisdj foisd f</p></td>
+    <td><img className="tableIMG" src={spaceIMG} alt="Project"/><p>Some test describing this thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj dfogj fdoigjf ojgoifd oifdj oigfdj  oigdjroig df oi gjfdoig fdo jfodj oifdj oifd oifd go idf oi gfdoig jfdoi foidsj foisdj foisd f</p></td>
   </tr>
+  </div>
   <tr>
-      <th colspan="2" className='tableTitle'>2023</th>
+      <th className='tableTitle'>2023</th>
       </tr>
+  <div className = "project-container">
   <tr>
     <th>PLACEHOLDER</th>
-    <th>Space Game</th>
   </tr>
   <tr>
-    <td><img className="tableIMG" src={spaceIMG} /><p>Some test describing this thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj dfogj fdoigjf ojgoifd oifdj oigfdj  oigdjroig df oi gjfdoig fdo jfodj oifdj oifd oifd go idf oi gfdoig jfdoi foidsj foisdj foisd f</p></td>
-    <td><img className="tableIMG" src={spaceIMG} /><p>Some test describing dfgoidfgsdfoijsdoifjsd oisdj oisdj f oisdj oisd oi sdoif jsdoi sdoi odf josdf osd oisd f oisdj f oisd joi oif jsodjf oisdf oisd f jthis thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdsdfosdf sod jfosd ois ofjsdo fosd fosdf osdjoif sd ffsgi jfdogo dfgoi foigjdfoi fdg oifdj oifdoi gdf oi dfo dofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj foidsj foisdj foisd f</p></td>
+    <td><img className="tableIMG" src={spaceIMG} alt="Project" /><p>Some test describing this thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj dfogj fdoigjf ojgoifd oifdj oigfdj  oigdjroig df oi gjfdoig fdo jfodj oifdj oifd oifd go idf oi gfdoig jfdoi foidsj foisdj foisd f</p></td>
   </tr>
+  </div>
   <tr>
-      <th colspan="2" className='tableTitle'>2022</th>
+      <th className='tableTitle'>2022</th>
       </tr>
+    <div className = "project-container">
   <tr>
     <th>PLACEHOLDER</th>
-    <th>Space Game</th>
   </tr>
   <tr>
-    <td><img className="tableIMG" src={spaceIMG} /><p>Some test describing this thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj dfogj fdoigjf ojgoifd oifdj oigfdj  oigdjroig df oi gjfdoig fdo jfodj oifdj oifd oifd go idf oi gfdoig jfdoi foidsj foisdj foisd f</p></td>
-    <td><img className="tableIMG" src={spaceIMG} /><p>Some test describing dfgoidfgsdfoijsdoifjsd oisdj oisdj f oisdj oisd oi sdoif jsdoi sdoi odf josdf osd oisd f oisdj f oisd joi oif jsodjf oisdf oisd f jthis thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdsdfosdf sod jfosd ois ofjsdo fosd fosdf osdjoif sd ffsgi jfdogo dfgoi foigjdfoi fdg oifdj oifdoi gdf oi dfo dofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj foidsj foisdj foisd f</p></td>
+    <td><img className="tableIMG" src={spaceIMG} alt="Project" /><p>Some test describing this thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj dfogj fdoigjf ojgoifd oifdj oigfdj  oigdjroig df oi gjfdoig fdo jfodj oifdj oifd oifd go idf oi gfdoig jfdoi foidsj foisdj foisd f</p></td>
   </tr>
+  </div>
   <tr>
-      <th colspan="2" className='tableTitle'>2020</th>
+      <th className='tableTitle'>2020</th>
       </tr>
+  <div className = "project-container">
   <tr>
     <th>Java TicTacToe</th>
-    <th>Older Java Projects</th>
   </tr>
   <tr>
-    <td><img className="tableIMG" src={spaceIMG} /><p>Some test describing this thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj dfogj fdoigjf ojgoifd oifdj oigfdj  oigdjroig df oi gjfdoig fdo jfodj oifdj oifd oifd go idf oi gfdoig jfdoi foidsj foisdj foisd f</p></td>
-    <td><img className="tableIMG" src={spaceIMG} /><p>Some test describing dfgoidfgsdfoijsdoifjsd oisdj oisdj f oisdj oisd oi sdoif jsdoi sdoi odf josdf osd oisd f oisdj f oisd joi oif jsodjf oisdf oisd f jthis thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdsdfosdf sod jfosd ois ofjsdo fosd fosdf osdjoif sd ffsgi jfdogo dfgoi foigjdfoi fdg oifdj oifdoi gdf oi dfo dofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj foidsj foisdj foisd f</p></td>
+    <td><img className="tableIMG" src={spaceIMG} alt="Project" /><p>Some test describing this thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj dfogj fdoigjf ojgoifd oifdj oigfdj  oigdjroig df oi gjfdoig fdo jfodj oifdj oifd oifd go idf oi gfdoig jfdoi foidsj foisdj foisd f</p></td>
   </tr>
+  </div>
 </table>
     </body>
 
