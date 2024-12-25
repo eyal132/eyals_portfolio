e52ea3af0bbd4b9ac5ad439ecb83509f8c9c5161
eyal132 <38598729+eyal132@users.noreply.github.com>
2024-12-25 15:52:57 +1300

    Bug Fixes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../App.css';
 import Navbar from './Nav';
-import eyalIMG from '../media/eyal-portrait.png';
+import eyalIMG from '../media/eyal.png';
 import spaceIMG from '../media/space_game.png';
+import spaceIMG2 from '../media/space_game2.png';
 import Cornerstone1 from '../media/Cornerstone1.png';
 import Cornerstone2 from '../media/Cornerstone2.png';
 import Cloud1 from '../media/Cloud1.png';
 import Cloud2 from '../media/Cloud2.png';
+import Radiation1 from '../media/radiation1.png';
+import Radiation2 from '../media/radiation2.png';
 
 function Home() {
 
@@ -54,7 +57,7 @@ function Home() {
         <p class="techCard">Github</p></div>
         </div>
         <img className="tableIMG" src={Cornerstone2} alt="Project" />
-        <div id="aboutProject"><h2 id="aboutTitle">About the Project</h2><p id="aboutText">Cornerstone was a final-year project designed to address a real-world challenge faced by our lecturers: the need for an improved project management and allocation system for the course. Following the successful deployment of the website, the project was honored with the title of "Best Computer Science Capstone Project 2024" at the University of Auckland.</p></div></td>
+        <div id="aboutProject"><h2 id="aboutTitle">About the Project</h2><p id="aboutText">Cornerstone was a final-year group project designed to address a real-world challenge faced by our lecturers: the need for an improved project management and allocation system for the course. Following the successful deployment of the website, the project was honored with the title of "Best Computer Science Capstone Project 2024" at the University of Auckland.</p></div></td>
   </tr>
   </div>
   <div className = "project-container">
@@ -79,14 +82,14 @@ function Home() {
     <th>Radiation Game</th>
   </tr>
   <tr>
-    <p id="date">Completion Year: 2023</p>
-    <td><img className="tableIMG" src={spaceIMG} alt="Project" /><div className='tech-stack-container'><h2 id="techStack">Tech Stack: </h2>
+    <p id="date">In Devlopment Since: 2023</p>
+    <td><img className="tableIMG" src={Radiation1} alt="Project" /><div className='tech-stack-container'><h2 id="techStack">Tech Stack: </h2>
     <div class="tech-items">
         <p class="techCard">Unity</p>
         <p class="techCard">C#</p></div>
         </div>
-        <img className="tableIMG" src={spaceIMG} alt="Project" />
-        <div id="aboutProject"><h2 id="aboutTitle">About the Project</h2><p id="aboutText">Some test describing this thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj dfogj fdoigjf ojgoifd oifdj oigfdj  oigdjroig df oi gjfdoig fdo jfodj oifdj oifd oifd go idf oi gfdoig jfdoi foidsj foisdj foisd f</p></div></td>
+        <img className="tableIMG" src={Radiation2} alt="Project" />
+        <div id="aboutProject"><h2 id="aboutTitle">About the Project</h2><p id="aboutText">This project is a horror-themed game designed to immerse players in a chilling and eerie atmosphere. With haunting sound effects and a focus on creating an unsettling environment, the game aims to deliver a linear, story-driven experience set in an abandoned nuclear power plant. The primary objective is to evoke a sense of suspense and dread, making the atmospheric design a central element of the gameplay.</p></div></td>
   </tr>
   </div>
   <div className = "project-container">
@@ -101,25 +104,8 @@ function Home() {
         <p class="techCard">C#</p>
         <p class="techCard">Blender</p></div>
         </div>
-        <img className="tableIMG" src={spaceIMG} alt="Project" />
-        <div id="aboutProject"><h2 id="aboutTitle">About the Project</h2><p id="aboutText">Some test describing this thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj dfogj fdoigjf ojgoifd oifdj oigfdj  oigdjroig df oi gjfdoig fdo jfodj oifdj oifd oifd go idf oi gfdoig jfdoi foidsj foisdj foisd f</p></div></td>
-  </tr>
-  </div>
-    <div className = "project-container">
-  <tr>
-    <th>PLACEHOLDER</th>
-  </tr>
-  <tr>
-    <p id="date">Completion Year: 2024</p>
-    <td><img className="tableIMG" src={spaceIMG} alt="Project" /><div className='tech-stack-container'><h2 id="techStack">Tech Stack: </h2>    
-    <div class="tech-items">
-        <p class="techCard">React</p>
-        <p class="techCard">NodeJS</p>
-        <p class="techCard">Express</p>
-        <p class="techCard">MongoDB</p></div>
-        </div>
-        <img className="tableIMG" src={spaceIMG} alt="Project" />
-        <div id="aboutProject"><h2 id="aboutTitle">About the Project</h2><p id="aboutText">Some test describing this thing and how I made it sdfuiosdjhf s sdfois dof sopfi jsdofij o sdof sdof jsodfj sodf jsodjfos djfsod fjsd fijsdfo isdjfoisdjfoisd jfoisd fo sfoisdj dfogj fdoigjf ojgoifd oifdj oigfdj  oigdjroig df oi gjfdoig fdo jfodj oifdj oifd oifd go idf oi gfdoig jfdoi foidsj foisdj foisd f</p></div></td>
+        <img className="tableIMG" src={spaceIMG2} alt="Project" />
+        <div id="aboutProject"><h2 id="aboutTitle">About the Project</h2><p id="aboutText">This project is an action-packed, story-driven space game that takes players on a journey through time and space. Players will engage in combat against enemies while uncovering the mystery behind an alien invasion of Earth. As the game progresses, players will advance through levels and their spaceship will become more powerful.</p></div></td>
   </tr>
   </div>
   <div className = "project-container">
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,8 +5,7 @@ function Navbar() {
     return (
         <section className="nav">
         <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="About">About</a></li>
+        <li><a href="/">Portfolio</a></li>
         <li><a href="Contact">Contact</a></li>
         </ul>
         </section>
